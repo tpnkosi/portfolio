494c561c44bd924e15b8b5a1139d44717d5314c0
thulilenkosi <tp.nkosi28@gmail.com>
2023-01-15 12:33:01 +0200

    removed vendor folder, added typed.js, updated CSS and HTML with code previously in vendor

---
+++ src/js/typed.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded",  ()=>{
+    //  Auto Type
+    let myText = new Typed(".typed",{
+      strings: ['Web Developer', 'Software Engineer', 'Java Developer'],
+      typeSpeed: 70,
+      backSpeed: 70,
+      loop: true
+    });
+    })
